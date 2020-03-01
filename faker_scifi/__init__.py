5518de3c3708772440dd416ba013749b37494908
Garison Spaeth <greesha@gmail.com>
2020-03-01 12:52:28 -0600

    initial add of provider

---
+++ faker_scifi/__init__.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+from faker.providers import BaseProvider
+
+
+class SciFi(BaseProvider):
+    gobbledygook = (
+        ('reverse',
+         'increase',
+         'absorb',
+         'melt',
+         'release',
+         'short circuit',
+         'calculate',
+         'use',
+         'bypass',
+         'inject',
+         'blow',
+         'instantiate',
+         'generate',
+         'induce',
+         'accelerate',
+         'close',
+         'modify',
+         'diable',
+         'heat',
+         'enhance',
+         'disengage',
+         'run'),
+        ('the polarity of the',
+         'the spacial',
+         'the epithelial',
+         'the warp',
+         'the miniaturization',
+         'the molecular',
+         'the time-space',
+         'the dimorphic',
+         'the life-sustaining',
+         'the submolecular',
+         'the quark',
+         'the pulse',
+         'the flux',
+         'the reality',
+         'the dilithium',
+         'the crystalline',
+         'the distortion',
+         'the quantum',
+         'the heavy-ion',
+         'a narrow beam of gravitonic',
+         'the external inertial'),
+        ('neutron flow',
+         'linkage',
+         'tissue',
+         'core',
+         'ray',
+         'overloader',
+         'coordinates',
+         'system',
+         'control',
+         'grid',
+         'parsecs',
+         'inertia',
+         'readings',
+         'vortex',
+         'energy',
+         'wave',
+         'emitters',
+         'field',
+         'radiation',
+         'fusion',
+         'dampener',
+         'mainframe'))
+
+    def gook(self):
+        """
+        :example 'reverse the polarity of the neutron flow'
+        """
+        result = []
+        for phrase in self.gobbledygook:
+            result.append(self.random_element(phrase))
+
+        return " ".join(result)
---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from codecs import open as fopen
+from os.path import dirname, abspath, join
+from setuptools import setup, find_packages
+
+VERSION = "0.1.0"
+
+DIR = dirname(abspath(__file__))
+
+with fopen(join(DIR, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = 'faker_scifi',
+    version = VERSION,
+    description = 'Faker provider for science fiction gobbledygook.',
+    long_description = long_description,
+    license = 'MIT',
+    keywords = 'faker faker-library faker-provider faker-generator scifi',
+    author = 'Garison Spaeth',
+    author_email = 'greesha@gmail.com',
+    url = 'https://github.com/scummchild/faker_scifi',
+    download_url = 'https://github.com/scummchild/faker_scifi/archive/v{0}.zip'.format(VERSION),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+    packages = find_packages(exclude=['docs', 'tests', 'tests.*']),
+    test_suite = 'test',
+    install_requires = [
+        'Faker',
+    ],
+)
