dab008fefacf391f868e3623275e6c858f69c44d
Garison Spaeth <greesha@gmail.com>
2020-03-24 21:09:35 -0500

    initial tests

---
+++ tests/test_faker_scifi.py
@@ -0,0 +1,17 @@
+import unittest
+
+from faker import Faker
+from faker_scifi import SciFi
+
+class TestEnUS(unittest.TestCase):
+    def setUp(self):
+        self.fake = Faker('en_US')
+        self.fake.add_provider(SciFi)
+        Faker.seed(0)
+
+    def test_babble(self):
+        assert self.fake.babble() == 'disengage the external inertial fluctuations'
+        assert self.fake.babble() == 'discharge the neutrino log'
+        assert self.fake.babble() == 'project the source of the energies'
+        assert self.fake.babble() == 'disrupt the anti- fields'
+        assert self.fake.babble() == 'experience the upper overloaders'
